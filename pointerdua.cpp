0f9ac006ea8d59599e8d673b610dd60660af0165
Hirzee <faruqhirzi698@gmail.com>
2026-04-27 13:46:34 +0700

    Memanggil method

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -16,6 +16,8 @@ int main()
     // Alokasi memori dinamis menggunakan 'new'
     mahasiswa *mhs = new mahasiswa{1}; // pointer Object mhs
     
-  
+    // Mengubah nilai nim melalui pointer
     mhs->nim = 2;
- 
+    
+  
+    mhs->showNim();
