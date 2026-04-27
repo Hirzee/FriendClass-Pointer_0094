ece58d789fd897f2b855540288abd060cbee9aec
Hirzee <faruqhirzi698@gmail.com>
2026-04-27 13:43:21 +0700

    Membuat objek mhs secara langsung

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -10,19 +10,6 @@ public:
 };
 
 int main() {
-    // Membuat objek mhs secara langsung
+    
     mahasiswa mhs{1}; // Object mhs
-    mhs.showNim();    // Member Access Operator (.)
-
-    // Menggunakan Reference (Alias)
-    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
-    refMhs.nim = 2;          // Member Access Operator
-    mhs.showNim();
-
-    // Menggunakan Pointer (Alamat Memori)
-    mahasiswa *pMhs = &mhs;  // Pointer Dereference pMhs
-    pMhs->nim = 3;           // Arrow Operator (->)
-    pMhs->showNim();
-
-    return 0;
-}
+    
